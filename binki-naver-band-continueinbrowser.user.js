3c661d63c834def763f94ec57eeb6777da8ae640
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2026-02-07 15:16:38 -0500

    fix @match to match www.band.us

--- binki-naver-band-continueinbrowser.user.js
+++ binki-naver-band-continueinbrowser.user.js
@@ -2,8 +2,7 @@
 // @name binki-naver-band-continueinbrowser
 // @homepageURL https://github.com/binki/binki-naver-band-continueinbrowser
 // @version 1.0.0
-// @match https://band.us/*
-// @require https://github.com/binki/binki-userscript-when-element-changed-async/raw/88cf57674ab8fcaa0e86bdf5209342ec7780739a/binki-userscript-when-element-changed-async.js
+// @match https://*.band.us/*
 // @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
 // ==/UserScript==
 
@@ -12,9 +11,5 @@
   while (true) {
     const buttonContinueInBrowser = await whenElementQuerySelectorAsync(document.body, buttonSelector);
     buttonContinueInBrowser.click();
-    
-    while (document.body.querySelector(buttonSelector)) {
-      await whenElementChangedAsync(document.body);
-    }
   }
 })();
