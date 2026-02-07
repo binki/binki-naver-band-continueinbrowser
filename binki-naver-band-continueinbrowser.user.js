145553f8212029326b845be706bb4d410106ec47
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2026-02-07 15:18:04 -0500

    binki-naver-band-continueinbrowser-1.0.1

--- binki-naver-band-continueinbrowser.user.js
+++ binki-naver-band-continueinbrowser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name binki-naver-band-continueinbrowser
 // @homepageURL https://github.com/binki/binki-naver-band-continueinbrowser
-// @version 1.0.0
+// @version 1.0.1
 // @match https://*.band.us/*
 // @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
 // ==/UserScript==
